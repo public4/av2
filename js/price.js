722158c9c71eb548240509927b9b331fdb476f04
Public4 <public4@ya.ru>
2018-10-11 17:29:37 +0500

    day 2 (11-10-2018). JSON load fix

--- js/price.js
+++ js/price.js
@@ -1,15 +1,16 @@
-var services = [];
+var services = []; //теперь сюда загружен список всех услуг
+var selected_services = []; //те услуги, которые выбраны
 
 
 function addService(id){
-    this.services.push(id);
-    $('#serviceList').append('<p>Service X <a href="#" onClick="removeService(1)"><span data-feather="x-circle"></span></a> </p>');
+    //this.services.push(id);
+    $('#serviceList').append('<p>'+this.services[id-1].service_name+`<a href="#" onClick="removeService(${id})"> <span data-feather="x-circle"></span></a></p>`);
     feather.replace()
     console.log(services);
 }
 
 function removeService(id){
-    this.services.slice(this.services.indexOf(id), 1);
+    //this.services.slice(this.services.indexOf(id), 1);
     console.warn('remove', id);
 }
 
@@ -17,13 +18,18 @@ function status(){
     console.log(this.services);
 }
 
+
 function getJ(){
-    $.getJSON('data.json', function(result){
-        $.each(result, function(k, v){
-            console.log(v);
-        })
+    let _data = [];
+    $.getJSON('data.json', function(data){
+        $.each(data, function(_key, _value){
+            _data.push(_value);
+        });
     });
-       
-   console.log('done');
-    
-  };
+    this.services = _data;
+    console.log('done', _data);
+}
+
+function showService(id){
+    console.log(this.services[id-1]);
+}
